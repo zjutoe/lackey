a029e9fb0d645b2a2bd5aa2aea26fe6c09b2f582
Zju Toe <zjutoe@gmail.com>
2014-03-04 23:48:00 +0800

    LaTeX hates dot . in fig file name, so use underline _

--- data/plot_regsync.sh
+++ data/plot_regsync.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-find . -name "*regsync*.eps" | xargs rm 
+find . -name "*regsync*_eps" | xargs rm 
 
-for f in *regsync*.log; do
+for f in *regsync*_log; do
 # set yr [0:250]
 gnuplot <<EOF
 load "plot_common.p"
 
 set ylabel "Reg Sync Push per Inst"
-set output "${f}.push.eps"
+set output "${f}_push.eps"
 plot "$f" using 1:5 title "Reg Sync Push per Inst" with lines lt 1
 
 EOF
 
 done
 
-for f in *regsync*.log; do
+for f in *regsync*_log; do
 # set yr [0:250]
 gnuplot <<EOF
 load "plot_common.p"
 
 set ylabel "Reg Sync Pull per Inst"
-set output "${f}.pull.eps"
+set output "${f}_pull.eps"
 plot "$f" using 1:7 title "Reg Sync Pull per Inst" with lines lt 1
 
 EOF
