2864bd6236e6eb751609ab92b979d496e242702c
John Ye <john.ye@intel.com>
2015-02-03 19:42:46 +0800

    mem reference trace for cache_sim_lua

---
+++ mtrace.lua
@@ -0,0 +1,44 @@
+-- Usage: 
+
+-- input: *.meta_rob.log
+-- output: 
+
+function __FILE__() return debug.getinfo(2,'S').source end
+function __LINE__() return debug.getinfo(2, 'l').currentline end
+
+local g_cid = 0
+
+function micro(m)   
+   if m.op == "L" then
+      print('R ' ..string.format("0x%x", m.i).. ' ' .. g_cid)
+   elseif m.op == "S" then
+      print('W ' ..string.format("0x%x", m.o).. ' ' .. g_cid)
+   end
+end
+
+function begin_sb(sb)
+   g_cid = sb.cid
+end
+
+function end_sb()
+end
+
+function begin_issue(issue)   
+end
+
+function end_issue()
+end
+
+
+local BUFSIZE = 2^15		-- 32K
+local f = io.input(arg[1])	-- open input file
+local cc, lc, wc = 0, 0, 0	-- char, line, and word counts
+while true do
+   local lines, rest = f:read(BUFSIZE, "*line")
+   if not lines then break end
+   if rest then lines = lines .. rest .. "\n" end
+
+   assert(loadstring(lines))()
+end
+
+
