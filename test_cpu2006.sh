60e2bedbe8ca575319db7ee7a57fbfc79c2619d9
John Ye <john.ye@intel.com>
2015-04-27 14:17:45 +0800

    test_cpu2006.sh: hardcoded home changed to /home/toe

--- test_cpu2006.sh
+++ test_cpu2006.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 inputs="\
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/astar_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/bzip2_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/gcc_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/gobmk_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/h264ref_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/hmmer_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/lbm_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/libquantum_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/mcf_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/milc_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/namd_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/omnetpp_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/perlbench_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/povray_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/sjeng_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/soplex_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/specrand_base.amd64-m64-gcc46-3.bin/mtrace.log \
-/Users/toe/doc/paper/p_shared_L1/data/cpu2006_log/sphinx_livepretend_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/astar_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/bzip2_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/gcc_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/gobmk_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/h264ref_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/hmmer_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/lbm_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/libquantum_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/mcf_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/milc_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/namd_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/omnetpp_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/perlbench_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/povray_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/sjeng_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/soplex_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/specrand_base.amd64-m64-gcc46-3.bin/mtrace.log \
+$HOME/doc/paper/p_shared_L1/data/cpu2006_log/sphinx_livepretend_base.amd64-m64-gcc46-3.bin/mtrace.log \
 "
 
 for input in $inputs; do
